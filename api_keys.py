18e68f9795e586be22e76b4eed721b29fd106d3c
Luke Fuller <lukefuller@Lukes-MacBook-Pro.local>
2021-10-17 13:11:23 -0400

    Update Api Keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "988e755da0041bda090d2a1b8216cee0"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBGJYhNH4DV-XGE179OJ1Evg0dVNnGAyCs"
